ccf099fc6c7ff119e0d98fa11a27f73ca102b35a
Fabiola Aguilera <fabiaguilera96@gmail.com>
2022-02-09 14:26:42 -0500

    fixing migration - databse column issue

--- AndromedaWave.Data/Product.cs
+++ AndromedaWave.Data/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,7 @@ namespace AndromedaWave.Data
     }
     public class Product
     {
-        [Key]
+        [Key, Column("TicketId")]
         public int TicketId { get; set; }
 
         [Required]
