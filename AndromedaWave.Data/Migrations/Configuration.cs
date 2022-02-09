765e6dff4462a54f3cdb2c805b6e6cb3fcdb4e52
Fabiola Aguilera <fabiaguilera96@gmail.com>
2022-02-09 14:20:37 -0500

    enabled auto migration dataloss to true

--- AndromedaWave.Data/Migrations/Configuration.cs
+++ AndromedaWave.Data/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace AndromedaWave.Data.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
             ContextKey = "AndromedaWave.Data.ApplicationDbContext";
         }
 
